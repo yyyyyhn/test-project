2604c31511acd886c99567c4c3a2d436f751dfd1
yuhaining <yhn19960101@163.com>
2020-12-06 17:38:18 +0800

    测试项目提交

--- src/main/java/com/yhn/shirodemo/controller/UserController.java
+++ src/main/java/com/yhn/shirodemo/controller/UserController.java
@@ -2,12 +2,15 @@ package com.yhn.shirodemo.controller;
 
 import com.yhn.shirodemo.test.Test1;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-//    @Autowired
-//    private Test1 test1;
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
 
 }
